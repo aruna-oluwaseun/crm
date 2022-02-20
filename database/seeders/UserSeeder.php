97a4b41f9ce0fcabdfc2022d97181f966006ad14
Aruna <arunaseun@yahoo.com>
2022-02-20 18:19:20 +0100

    seeder error fixed

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -43,10 +43,6 @@ class UserSeeder extends Seeder
             ]
         ]);
 
-        DB::table('role_user')->insert([
-            ['user_id' => 1, 'role_id' => 1],
-            ['user_id' => 2, 'role_id' => 1]
-        ]);
-
+       
     }
 }
